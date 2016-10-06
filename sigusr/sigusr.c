25b6b1696d48f5620198f4229a774c9e65c3c3b8
AYaNikita <botva96@mail.ru>
2016-10-06 21:03:01 +0400

    Update sigusr.c

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <signal.h>
 
-extern const char * const sys_siglist[];
-void my_handler(int sig,siginfo_t *siginfo,void *context) {
-	printf("%s from %ld",sys_siglist[sig],(long)siginfo->si_pid);
+void handler(int signal,siginfo_t *signalinfo,void *context) {
+        if( signal == SIGUSR1 )
+		printf("SIGUSR1 from %ld",(long)signalinfo->si_pid);
+	else
+		printf("SIGUSR2 from %ld",(long)signalinfo->si_pid);
 	exit(0);
 }
 
+
 int main(int argc,char* argv[]) {
+
 	struct sigaction sigact;
-	sigact.sa_sigaction = &my_handler;
+	sigact.sa_sigaction = &handler;
 	sigset_t block_mask;
 	sigemptyset(&block_mask);
 	sigaddset(&block_mask,SIGUSR1);
@@ -20,8 +24,7 @@ int main(int argc,char* argv[]) {
 	sigact.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1,&sigact,NULL);
 	sigaction(SIGUSR2,&sigact,NULL);
-        sleep(10);
-	
+        sleep(10);	
 	printf("No signals were caught");
 	return 0;
 }

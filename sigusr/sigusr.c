e368eb59f60bc2c92b80eda251a6905df0ed71e2
AYaNikita <botva96@mail.ru>
2016-10-06 14:24:29 +0300

    Update sigusr.c

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -1,38 +1,27 @@
-#include <unistd.h>
-#include <signal.h>
-#include <sys/wait.h>
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 
-volatile sig_atomic_t was = 0;
-
-void handler(int signo, siginfo_t *siginfo, void *context) {
-	was = 1;
-	printf("signal %d from: %d\n", signo, (int)(siginfo->si_pid));
-	exit(signo);
+extern const char * const sys_siglist[];
+void my_handler(int sig,siginfo_t *siginfo,void *context) {
+	printf("%s from %ld",sys_siglist[sig],(long)siginfo->si_pid);
+	exit(0);
 }
 
-int main() {
-    	struct sigaction sigact;
-    	sigact.sa_sigaction = *handler;
-    	sigact.sa_flags |= SA_SIGINFO;
-    	sigset_t block_mask;
-    	sigfillset(&block_mask);
+int main(int argc,char* argv[]) {
+	struct sigaction sigact;
+	sigact.sa_sigaction = &my_handler;
+	sigset_t block_mask;
+	sigemptyset(&block_mask);
+	sigaddset(&block_mask,SIGUSR1);
+	sigaddset(&block_mask,SIGUSR2);
 	sigact.sa_mask = block_mask;
-	int i;
-	for (i = 1; i < 32; ++i) {
-		if (((i == SIGKILL) || (i == SIGSTOP))) continue;
-		if (sigaction(i, &sigact, NULL) != 0) {
-        		printf("%d error\n", i);
-       		 	return errno;
-    		}
-	} 
-   	sleep(10);
-	sigprocmask(SIG_BLOCK, &sigact.sa_mask, 0);
-	if (!was) {
-    		printf("No signals were caught\n");
-	}
-    	return 0;
+	sigact.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1,&sigact,NULL);
+	sigaction(SIGUSR2,&sigact,NULL);
+        sleep(10);
+	
+	printf("No signals were caught");
+	return 0;
 }
